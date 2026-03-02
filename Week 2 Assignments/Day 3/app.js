a251478ae720531894058f056d977496a2eb9f73
aripiralachaitanya <aripiralachaitanya3@gmail.com>
2026-03-02 23:24:47 +0530

    Delete Week 2 Assignments/Day 3/app.js

--- Week 2 Assignments/Day 3/app.js
+++
@@ -1,5 +0,0 @@
-import {newtask,display,completeTask} from './taskoperation.js'
-console.log(newtask(1,"adi",'low','12-01-2025',"completed"))
-console.log(newtask(2,"adi",'high','01-12-2025',"not completed"))
-console.log(newtask(3,"sdf",'edef','01-12-2025','not completed'))
-console.log(display())
