bdeec11d60d3963ce7d79a9063f9b9bf840c2f29
aripiralachaitanya <aripiralachaitanya3@gmail.com>
2026-03-03 13:43:19 +0530

    Delete Week1_Assignment/day1_4.js

--- Week1_Assignment/day1_4.js
+++
@@ -1,8 +0,0 @@
-//Find the smallest element in marks array
-let marks=[90,78,65,98]
-let smallest=marks[0]
-for(let i=1;i<marks.length;i++){
-    if(marks[i]<smallest){
-        smallest=marks[i]
-    }}
-console.log("The smallest mark is",smallest)
